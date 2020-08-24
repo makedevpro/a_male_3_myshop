22afbedcfcb7b148da7ab8896d07f9d6a71389f5
makedevpro <makedevpro@gmail.com>
2020-08-24 11:29:17 +0300

    7. Added add_cart button(this form to template detail) and FIX cast urls cart_add, and FIX literal error in template cart detail

--- cart/views.py
+++ cart/views.py
@@ -17,7 +17,7 @@ def cart_add(request, product_id):
         cart.add(product=product,
                  quantity=cd['quantity'],
                  update_quantity=cd['update'])
-        return redirect('cart:cart:detail')
+        return redirect('cart:cart_detail')
 
 
 def cart_remove(request, product_id):

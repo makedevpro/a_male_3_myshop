e3a9c7c829be4ece971fb323cb6fafe039afbfc2
makedevpro <makedevpro@gmail.com>
2020-08-24 11:53:34 +0300

    7. Added update_quantity_form to view for change quantity product in the cart

--- cart/views.py
+++ cart/views.py
@@ -31,4 +31,9 @@ def cart_remove(request, product_id):
 def cart_detail(request):
     """ Отображаем корзину, основывая на данных сохраненных в сессии """
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={'quantity': item['quantity'],
+                     'update': True}
+        )
     return render(request, 'cart/detail.html', {'cart': cart})

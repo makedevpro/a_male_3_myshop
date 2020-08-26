48e76ca7562547f2f06fb72c31838562ea832caf
makedevpro <makedevpro@gmail.com>
2020-08-26 14:54:32 +0300

    9. Added prefix in url and change language on page

--- a_male_3_myshop/urls.py
+++ a_male_3_myshop/urls.py
@@ -14,19 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.utils.translation import gettext_lazy as _
 
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('rosetta/', include('rosetta.urls')),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
-    path('payments/', include('payment.urls', namespace='payment')),
-    path('coupons/', include('coupons.urls', namespace='coupons')),
-    path('admin/', admin.site.urls),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('orders/'), include('orders.urls', namespace='orders')),
+    path(_('payments/'), include('payment.urls', namespace='payment')),
+    path(_('coupons/'), include('coupons.urls', namespace='coupons')),
+    path(_('admin/'), admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', include('shop.urls', namespace='shop')),
-]
+    prefix_default_language=False
+)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
